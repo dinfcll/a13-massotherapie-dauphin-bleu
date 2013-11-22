c4226b2003d85774bc014d8d7b3eaef0c9777884
administrateur <olivier_duhamel@hotmail.com>
2013-11-22 11:46:29 -0500

    aa

--- index.php
+++ index.php
@@ -10,3 +10,5 @@ include("header.html");
 
 ?>
 
+<iframe width="640" height="360" src="//www.youtube.com/embed/52btV4HjuFQ?feature=player_embedded" frameborder="0" allowfullscreen></iframe>
+
--- service.php
+++ service.php
@@ -8,3 +8,38 @@
  */
 
 include("header.html");
+
+?>
+<style>
+    .marge{
+        margin:100px;
+    }
+
+</style>
+<div class=marge>
+<h3>Choix de services et tarifs &agrave; la carte:</h3><br/>
+    <h4>plage horaire:</h4><br/>
+am : 9h00 - 11h30<br/>
+pm : 13h30 - 16h00<br/>
+soir : 19h00 - 21h30<br/>
+<br/>
+<h4>Choix de forfait:<br/></h4>
+<br/>
+R&eacute;servation forfait - massage pr&eacute;natal<br/>
+R&eacute;servation forfait - massage postnatal<br/>
+R&eacute;servation forfait - massage des parents<br/>
+R&eacute;servation forfait - massage des parents et enfants<br/>
+
+<h4>Services &agrave; la carte :</h4><br/>
+
+Massage pr&eacute;natal - 70 $<br/>
+Massage postnatal - 70 $<br/>
+Massage postnatal et papa masse b&eacute;b&eacute; 80 $<br/>
+Massage des parents 140 $<br/>
+Massage pr&eacute;natal et 1 massage enfant 80 $<br/>
+Massage pr&eacute;natal et 2 massages enfant 90$<br/>
+Massage postnatal et 1 massage enfant 80 $<br/>
+Massage postnatal et 2 massages enfant 90 $<br/>
+Massage famille 150 $<br/>
+Massage famille (Bassin des dauphins) - 125 $<br/><br/>
+</div>
