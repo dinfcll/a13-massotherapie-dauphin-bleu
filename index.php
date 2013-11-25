12900b298194de442a48d0686bac7e692550eb95
administrateur <olivier_duhamel@hotmail.com>
2013-11-25 11:06:56 -0500

    aa

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: administrateur
- * Date: 13-09-14
- * Time: 04:42
- * To change this template use File | Settings | File Templates.
- */
+
 include("header.html");
 
 ?>
 <div class="container">
-<iframe width="640" height="360" src="//www.youtube.com/embed/52btV4HjuFQ?feature=player_embedded" frameborder="0" allowfullscreen></iframe>
+    Massoth&eacute;rapie Le dauphin bleu est une entreprise de la r&eacute;gion de Qu&eacute;bec qui offrent des services &agrave; domicile sp&eacute;cialis&eacute;e en p&eacute;rinatalit&eacute;.  Massages pr&eacute;natal, postnatal, papa apprend à masser b&eacute;b&eacute;, le massage des parents en duo et le massage famille.
+    Le forfait &laquo; Le shower &raquo; est abordable et complet.  5 services en 3 rencontres, 1 sac cadeau  et un massage de 30 minutes pour les autres enfants de la famille.
+    Les familles qui profiteront du forfait &laquo; Le shower &raquo; deviennent membre &agrave; vie du Bassin des dauphins pour une r&eacute;duction de 25 $ sur le prix r&eacute;gulier du massage famille.
+    <br/><br/>&Eacute;mission de re&ccedil;us pour assurances.
+    <br/><br/>
+
+    <iframe width="640" height="360" src="//www.youtube.com/embed/52btV4HjuFQ?feature=player_embedded" frameborder="0" allowfullscreen></iframe>
 </div>
