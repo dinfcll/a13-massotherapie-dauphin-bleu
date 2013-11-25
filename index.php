df594b9e2053b9e2c3cfdc620f7242c289eee882
administrateur <olivier_duhamel@hotmail.com>
2013-11-25 12:02:03 -0500

    aa

--- index.php
+++ index.php
@@ -12,4 +12,7 @@ include("header.html");
     <br/><br/>
 
     <iframe width="640" height="360" src="//www.youtube.com/embed/52btV4HjuFQ?feature=player_embedded" frameborder="0" allowfullscreen></iframe>
+
+    <br/><br/>
+    <h3>Quelques photos!</h3>
 </div>
